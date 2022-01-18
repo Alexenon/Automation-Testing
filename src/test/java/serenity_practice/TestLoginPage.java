f23bda87b51008cc26e70d3031fb5ec3befb5f60
Alex Xenon <b3haply@gmail.com>
2022-01-18 22:39:50 +0200

    Added serenity runner class but now it doesn't work, throws an exception

--- src/test/java/serenity_practice/TestLoginPage.java
+++ src/test/java/serenity_practice/TestLoginPage.java
@@ -12,24 +12,20 @@ public class TestLoginPage extends PageObject {
     private final By inputPassword = By.xpath("//input[@id='user_password']");
     private final By BtnSubmitLogin = By.xpath("//input[@value='Login']");
 
-    public TestLoginPage userClickLoginButton(){
+    public void userClickLoginButton(){
         find(BtnLogin).click();
-        return this;
     }
 
-    public TestLoginPage userInsertEmail(String email){
+    public void userInsertEmail(String email){
         find(inputEmail).sendKeys(email);
-        return this;
     }
 
-    public TestLoginPage userInsertPassword(String password){
+    public void userInsertPassword(String password){
         find(inputPassword).sendKeys(password);
-        return this;
     }
 
-    public TestLoginPage userSubmitLogin(){
+    public void userSubmitLogin(){
         find(BtnSubmitLogin).click();
-        return this;
     }
 
 
