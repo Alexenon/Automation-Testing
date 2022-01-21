25477bd79bbe160ef0e2c82af9a5e8373d661855
Alex Xenon <b3haply@gmail.com>
2022-01-21 22:35:37 +0200

    Added serenity runner class but now it doesn't work, throws an exception

--- src/test/java/cucumber_practice/TestRunner.java
+++ src/test/java/cucumber_practice/TestRunner.java
@@ -1,8 +1,10 @@
 package cucumber_practice;
 
 import cucumber.api.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.Class)
+@RunWith(Cucumber.class)
 @CucumberOptions(features="", glue="")
 public class TestRunner {
 
